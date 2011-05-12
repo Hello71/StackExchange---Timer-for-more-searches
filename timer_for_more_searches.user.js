81a92b23b5864571b27380754db3ccae9606e3b6
Alex Xu <alex@alex-desktop>
2011-05-12 17:17:04 -0400

    fixed for next push

--- timer_for_more_searches.user.js
+++ timer_for_more_searches.user.js
@@ -12,7 +12,7 @@
 // ==/UserScript==
 (function () {
 	var error = document.querySelector(".page-description > p");
-	if (error !== null && error.textContent === "You can only perform 6 searches within a minute, please wait a minute then try again.") {
+	if (error !== null && (error.textContent === "You can only perform 6 searches within a minute, please wait a minute then try again." || error.textContent === "You can only perform 6 searches within a 60 second window, please wait a moment and try again.")) {
 		var remaining = 60 - new Date().getMinutes();
 		error.textContent = "You can only perform 6 searches within a minute, please wait " + remaining-- + " seconds then try again.";
 		window.setInterval(function () {
