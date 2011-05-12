b3422e79c0a5407baaf355d09e5aa116ce62787d
Alex Xu <alex@alex-desktop>
2011-05-12 17:34:27 -0400

    added google suggestion

--- timer_for_more_searches.user.js
+++ timer_for_more_searches.user.js
@@ -13,6 +13,17 @@
 (function () {
 		var error = document.querySelector(".page-description > p");
 		if (error !== null && (error.textContent === "You can only perform 6 searches within a minute, please wait a minute then try again." || error.textContent === "You can only perform 6 searches within a 60 second window, please wait a moment and try again.")) {
+				var google = document.createElement("p");
+				google.appendChild(document.createTextNode("Alternatively, try your search in Google:"));
+				error.parentNode.appendChild(google);
+				google = google.cloneNode(false);
+				var query = window.location.search.replace("?q=", "http://www.google.com/search?q=site:stackoverflow.com/questions+");
+				var googlelink = document.createElement("a");
+				googlelink.href = query;
+				googlelink.appendChild(document.createTextNode(query.replace("+", " ")));
+				googlelink.setAttribute("style", "font-weight: bold;");
+				google.appendChild(googlelink);
+				error.parentNode.appendChild(google);
 				var remaining = 60 - new Date().getMinutes();
 				var print = function () {
 						error.textContent = "You can only perform 6 searches within a minute, please wait " + remaining + " seconds then try again.";
