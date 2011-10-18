649495d13973df0ad8db774374376a15ba33fb3a
Alex Xu <alex_y_xu@yahoo.ca>
2011-10-18 19:50:20 -0400

    fixed a lot of bugs, retabbed, possibly increased speed, more accurate timer, removed old string

--- timer_for_more_searches.user.js
+++ timer_for_more_searches.user.js
@@ -1,33 +1,41 @@
 // ==UserScript==
-// @name           Timer for more searches
+// @name    Timer for more searches
 // @namespace      http://stackexchange.com
-// @include        http://*.stackexchange.com/search?q=*
-// @include        http://stackoverflow.com/search?q=*
-// @include        http://superuser.com/search?q=*
-// @include        http://serverfault.com/search?q=*
-// @include        http://askubuntu.com/search?q=*
-// @include        http://meta.stackoverflow.com/search?q=*
-// @include        http://answers.onstartups.com/search?q=*
-// @include        http://stackapps.com/search?q=*
+// @include     http://*.stackexchange.com/search?q=*
+// @include     http://stackoverflow.com/search?q=*
+// @include     http://superuser.com/search?q=*
+// @include     http://serverfault.com/search?q=*
+// @include     http://askubuntu.com/search?q=*
+// @include     http://meta.stackoverflow.com/search?q=*
+// @include     http://answers.onstartups.com/search?q=*
+// @include     http://stackapps.com/search?q=*
 // ==/UserScript==
 (function () {
-		var error = document.querySelector(".page-description > p");
-		if (error !== null && (error.textContent === "You can only perform 6 searches within a minute, please wait a minute then try again." || error.textContent === "You can only perform 6 searches within a 60 second window, please wait a moment and try again.")) {
-				var query = window.location.search.replace("?q=", "http://www.google.com/search?q=site:stackoverflow.com/questions+");
-				error.innerHTML += "<p>Alternatively, try your search in Google:</p><p><b><a href=\"" + query + "\">" + query.replace("+", " ") + "</a></b></p>";
-				var remaining = 60 - new Date().getSeconds();
-				var print = function () {
-						var text = "You can only perform 6 searches within a 60 second window, please wait " + remaining + " second";
-						if (remaining !== 1) {
-								text += "s";
-						}
-						error.textContent = text + " and try again.";
-				}
-				print();
-				window.setInterval(function () {
-						if (remaining-- > 0) {
-								print();
-						}
-				}, 1000);
-		}
+    var error = document.querySelector(".page-description > p");
+    if (error !== null && error.textContent === "You can only perform 6 searches within a 60 second window, please wait a moment and try again.") {
+        var search = window.location.search;
+        var q = search.indexOf("q=") + 2;
+        var google = "http://www.google.com/search?q=site:stackoverflow.com/questions+" + search.substring(q, search.indexOf("&", q));
+        error.innerHTML = "<p>You can only perform 6 searches within a 60 second window, please wait <span id='timeRemaining'></span> and try again.</p><p>Alternatively, try your search in Google:</p><p><b><a href=\"" + google + "\">" + google.replace("+", " ") + "</a></b></p>";
+        var timeRemaining = document.getElementById("timeRemaining");
+        var remaining = 60 - new Date().getSeconds();
+        var timer;
+        var print = function (a) {
+            if (a === true) {
+                timer = window.setInterval(print, 1000);
+            }
+            remaining--;
+            if (remaining === 1) {
+                timeRemaining.textContent = "1 second";
+            } else if (remaining > 0) {
+                timeRemaining.textContent = remaining + " seconds";
+            } else {
+                window.clearInterval(timer);
+            }
+        };
+        print(false);
+        window.setTimeout(function () {
+            print(true);
+        }, 1000 - (new Date().milliseconds % 1000));
+    }
 }());
